82aa4c455c1ea94329b5e1212af89d33cd01869f
ROHIT318 <iro1hit@gmail.com>
2022-03-10 23:58:11 +0530

    Registration form creation and database connection

--- camera/models.py
+++ camera/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class registration_form(models.Model):
+	unique_id = model.charField(max_length=10, primary_key=True)
+	name = model.charField(max_length=60)
+	img_1 = models.ImageField(upload_to='pics')
+	img_2 = models.ImageField(upload_to='pics')
+	img_3 = models.ImageField(upload_to='pics')
+	img_4 = models.ImageField(upload_to='pics')
+	img_5 = models.ImageField(upload_to='pics')
