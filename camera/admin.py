79115e3ccf6dc58c75434093a3210fbd2b6ae458
ROHIT318 <iro1hit@gmail.com>
2022-04-04 22:43:01 +0530

    Modified Admin Panel

--- camera/admin.py
+++ camera/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import registration_form
+from .models import *
 
-admin.site.register(registration_form) 
+admin.site.register(registration_form)
+admin.site.register(Courses)
+
+class AdminAttendance_db(admin.ModelAdmin):
+	readonly_fields = ('time', )
+admin.site.register(Attendance_db, AdminAttendance_db)
+
+admin.site.register(Class_history)
